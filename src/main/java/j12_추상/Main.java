6b8a13e45ab6d65cfe127852330718fc027b8fc1
NALASAI <ebyul96428@naver.com>
2022-11-23 13:46:59 +0900

    챕터12추상 나머지

---
+++ src/main/java/j12_추상/Main.java
@@ -0,0 +1,26 @@
+package j12_추상;
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        int[][] arr = new int[100][100];
+
+        int count = 0;
+        for(int i = 0; i < num; i++){
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+
+            for( int j = x; j < x; j++){
+                for(int k = y; k < y+10; k++) arr[j][k] = 1;
+            }
+        }
+        for(int i = 0; i < 100; i++){
+            for(int j = 0; j < 100; j++){
+                if(arr[i][j] == 1) count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
