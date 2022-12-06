c4ca938cc6abdf098fa1012f4be1d70d8d75a0cd
NALASAI <ebyul96428@naver.com>
2022-12-06 14:25:29 +0900

    SQL 시작(Insert)

---
+++ SQLfile/Insert.sql
@@ -0,0 +1,17 @@
+/* 
+Create Read Update Delete
+
+Insert ( 추가 ) - C
+Select ( 조회 ) - R
+Update ( 수정 ) - U
+Delete ( 삭제 ) - D
+
+DB 에서는 작은따음표 사용, 스네이크 표기법 사용
+*/
+
+INSERT INTO product_mst
+(id, product_name, product_price)
+VALUES
+(0, '스타벅스 블랙  머그컵', 15000),
+(0, '스타벅스 화이트 머그컵', 20000),
+(0, '스타벅스 블루 머그컵', 15000);
